243f0d73b9eb5ac6411fce9d330e93580e526afd
Daniil Ahremenkow <kwillum@oa-d5.msk.21-school.ru>
2021-01-24 05:36:00 +0300

    provide extra tests

--- day07/ex02/main.cpp
+++ day07/ex02/main.cpp
@@ -6,6 +6,37 @@
 int main(void)
 {
 	Array<int> a(20);
+	Array<float> bl;
+	std::cout << "Size: " << bl.size() << std::endl << "\033[32mTry to call bl[0]: \033[31m";
+	try
+	{
+		std::cout << bl[0];
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	std::cout << "\033[0m";
+
+	std::cout << "\033[32mTry to call bl[-1]: \033[31m";
+	try
+	{
+		std::cout << bl[-1];
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	std::cout << "\033[0m";
+	Array<float> bly(5);
+	bl = bly;
+	std::cout << "\033[33mbl Array after assignment: \033[34m";
+	std::cout << "bl -> ";
+	for (size_t i = 0; i < bl.size(); i++)
+		std::cout << bl[i] << " ";
+	std::cout << std::endl;
+	std::cout << "\033[0m";
+
 
 	std::cout << "A -> ";
 	for (size_t i = 0; i < a.size(); i++)
